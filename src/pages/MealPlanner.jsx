352bd7c20ba69a107f7bede738aa3fd8ee30bb7c
alpbrace <ubeyde.alpkilic25@gmail.com>
2022-11-15 18:32:56 +0100

    meal planner

--- src/pages/MealPlanner.jsx
+++ src/pages/MealPlanner.jsx
@@ -1,27 +1,39 @@
 import React from "react";
 import styled from "styled-components";
-import coming from "../img/SL-043020-30500-40.jpg"
+import coming from "../img/SL-043020-30500-40.jpg";
 
-const Container =styled.div`
-width: 100%;
-height: 90vh;
-text-align: center;
-
-img{
+const Container = styled.div`
   width: 100%;
-  height: 100%;
-  object-fit: cover;
-  margin: auto;
-}
-@media (max-width: 800px) {
-  img{
- 
+  height: 89.3vh;
+  text-align: center;
+  position: relative;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    margin: auto;
+  }
+  @media (max-width: 800px) {
+    img {
+      object-fit: cover;
+    }
+  }
 
-  object-fit: cover;
-}
-}
-
-`
+  a {
+    position: absolute;
+    background-color: white;
+    bottom: 10px;
+    right: 0;
+    text-decoration: none;
+    color: black;
+    font-size: 0.8rem;
+  }
+`;
 export const MealPlanner = () => {
-  return (<Container><img src={coming} alt="coming" /></Container>);
+  return (
+    <Container>
+      <img src={coming} alt="coming" />
+      <a href="https://de.freepik.com/vektoren-kostenlos/im-bau-mit-schwarzen-und-gelben-streifen_13197704.htm#query=under%20construction&position=0&from_view=keyword" target={"_blank"}>Bild von starline auf Freepik</a> 
+    </Container>
+  );
 };
